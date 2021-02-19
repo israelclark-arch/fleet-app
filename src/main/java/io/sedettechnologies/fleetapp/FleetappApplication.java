6fe0f4014691e3f344160e992e35d2ebde165870
Israel Clark <clarkisraelofficial@gmail.com>
2021-02-19 16:30:03 +0100

    initialized my spring application and added my controllers, models, repositories and services packages

---
+++ src/main/java/io/sedettechnologies/fleetapp/FleetappApplication.java
@@ -0,0 +1,13 @@
+package io.sedettechnologies.fleetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/sedettechnologies/fleetapp/FleetappApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sedettechnologies.fleetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
